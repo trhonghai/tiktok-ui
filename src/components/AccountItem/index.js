7cd2daccb8f39fb61b2788b6cb0b9e08234d44f2
trhonghai <trhonghaii@gmail.com>
2023-12-05 14:39:22 +0700

    Xây dựng Logic phần Header #2

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,28 +1,27 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <img
-                className={cx('avatar')}
-                src="https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/aafac68c65b5eec6d06c31d05e27a48f~c5_720x720.jpeg?x-expires=1701435600&x-signature=EAqAf1bzJNKC2XuChz0pYBZ6mw4%3D"
-                alt="Hai"
-            />
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <img className={cx('avatar')} src={data.avatar} alt="Hai" />
             <div className={cx('infor')}>
                 <h4 className={cx('name')}>
-                    <span>Truong Hong Hai</span>
-                    <FontAwesomeIcon
-                        className={cx('check')}
-                        icon={faCheckCircle}
-                    />
+                    <span>{data.full_name}</span>
+                    {data.tick && (
+                        <FontAwesomeIcon
+                            className={cx('check')}
+                            icon={faCheckCircle}
+                        />
+                    )}
                 </h4>
-                <span className={cx('username')}>trhonghai</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
