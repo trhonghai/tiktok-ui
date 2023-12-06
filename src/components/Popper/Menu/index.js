0985f9ea647a7cd53e5e715b56853e6fa1b9a992
trhonghai <trhonghaii@gmail.com>
2023-12-06 10:36:12 +0700

    Sửa lỗi và hoàn thiện phần Header #2

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -58,7 +58,7 @@ function Menu({
                                 }
                             />
                         )}
-                        {renderItems()}
+                        <div className={cx('menu-body')}>{renderItems()}</div>
                     </PopperWrapper>
                 </div>
             )}
